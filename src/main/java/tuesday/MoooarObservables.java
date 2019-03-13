4b5077eb67c53567ce4e0327e8adc689788f7d19
John Rodriguez <jrod@squareup.com>
2019-03-12 23:52:15 -0400

    04:30: Add basic callbacks + pass to giveMeSomeData()

--- src/main/java/tuesday/MoooarObservables.java
+++ src/main/java/tuesday/MoooarObservables.java
@@ -2,5 +2,33 @@ package tuesday;
 
 public class MoooarObservables {
   public static void main(String[] args) {
+    NextCallback<Object> nextCallback = o -> { };
+
+    ErrorCallback errorCallback = throwable -> { };
+
+    CompleteCallback completeCallback = () -> { };
+
+    giveMeSomeData(
+      nextCallback,
+      errorCallback,
+      completeCallback
+    );
   }
+
+  static void giveMeSomeData(
+      NextCallback<Object> next, ErrorCallback error, CompleteCallback complete
+  ) {
+  }
+}
+
+interface NextCallback<T> {
+  void nextCallback(T t);
+}
+
+interface ErrorCallback {
+  void errorCallback(Throwable throwable);
+}
+
+interface CompleteCallback {
+  void completeCallback();
 }
